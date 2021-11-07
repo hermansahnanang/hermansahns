37bd2a174af4bbb20ba06b764e52de78ff338b9b
hermansahnanang <93857491+hermansahnanang@users.noreply.github.com>
2021-11-07 14:50:58 +0700

    Update ell.sh

--- ell.sh
+++ ell.sh
@@ -5,7 +5,9 @@ apt-get install sudo
 apt-get install git -y
 sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
 git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
-cd /ccminer
+ls Makefile
+Makefile cd ccminer
+cd /path/to/project ./ccminer
 chmod +x build.sh
 chmod +x configure.sh
 chmod +x autogen.sh
